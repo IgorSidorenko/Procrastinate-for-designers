21b1bed315658f770db1a1e93a1bbba6106b85d9
Igor <igorsid38@mail.ru>
2020-08-02 19:16:02 +0800

    cycle image

--- script.js
+++ script.js
@@ -9,26 +9,31 @@ let phrases = [
   { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
   { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
   { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
-  ];
-  
-  function getRandomElement(arr) {
-    let randIndex = Math.floor(Math.random() * arr.length);
-    return arr[randIndex];
+];
+
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
+
+let button = document.querySelector('.button');
+let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
+
+button.addEventListener('click', function () {
+  let randomElement = getRandomElement(phrases);
+  smoothly(phrase, 'textContent', randomElement.text)
+  smoothly(image, 'src', randomElement.image)
+
+  if (randomElement.text.length > 40) {
+    advice.style.fontSize = '33px';
+  } else {
+    advice.style.fontSize = '42px';
   }
-  
-  let button = document.querySelector('.button');
-  let phrase = document.querySelector('.phrase');
-  let advice = document.querySelector('.advice');
-  let image = document.querySelector('.image');
-  
-  button.addEventListener('click', function () {
-    let randomElement = getRandomElement(phrases);
-    phrase.textContent = randomElement;
-  
-    if (randomElement.length > 40) {
-      advice.style.fontSize = '33px';
-    } else {
-      advice.style.fontSize = '42px';
-    }
-  });
-  
+});
+
+for (let i = 0; i <= 2; i = i + 1) {
+  smoothly(phrase, 'textContent', phrases[i].text);
+  smoothly(image, 'src', phrases[i].image);
+}
